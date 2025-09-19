a3885d0d325a114b291a8a8c0293f8f354abc1df
Rafael Costa <rafaelcost@gmail.com>
2025-09-19 08:24:11 -0300

    Initial commit

---
+++ src/main/java/com/senai/conta_bancaria_turma2/ContaBancariaTurma2Application.java
@@ -0,0 +1,13 @@
+package com.senai.conta_bancaria_turma2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContaBancariaTurma2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ContaBancariaTurma2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/senai/conta_bancaria_turma2/ContaBancariaTurma2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.senai.conta_bancaria_turma2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ContaBancariaTurma2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
