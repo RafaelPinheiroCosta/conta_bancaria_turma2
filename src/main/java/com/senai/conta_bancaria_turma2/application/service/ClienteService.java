414ccd337bd7be38092031aef43e0f4f35773869
Rafael Costa <rafaelcost@gmail.com>
2025-09-26 10:04:35 -0300

    DTO

--- src/main/java/com/senai/conta_bancaria_turma2/application/service/ClienteService.java
+++ src/main/java/com/senai/conta_bancaria_turma2/application/service/ClienteService.java
@@ -40,4 +40,30 @@ public class ClienteService {
                 .map(ClienteResponseDTO::fromEntity)
                 .toList();
     }
+
+    public ClienteResponseDTO buscarClienteAtivoPorCpf(String cpf) {
+        var cliente = repository.findByCpfAndAtivoTrue(cpf).orElseThrow(
+                () -> new RuntimeException("Cliente não encontrado.")
+        );
+        return ClienteResponseDTO.fromEntity(cliente);
+    }
+
+    public ClienteResponseDTO atualizarCliente(String cpf, ClienteRegistroDTO dto) {
+        var cliente = repository.findByCpfAndAtivoTrue(cpf).orElseThrow(
+                () -> new RuntimeException("Cliente não encontrado.")
+        );
+
+        cliente.setNome(dto.nome());
+        cliente.setCpf(dto.cpf());
+
+        return ClienteResponseDTO.fromEntity(repository.save(cliente));
+    }
+
+    public void deletarCliente(String cpf) {
+        var cliente = repository.findByCpfAndAtivoTrue(cpf).orElseThrow(
+                () -> new RuntimeException("Cliente não encontrado.")
+        );
+        cliente.setAtivo(false);
+        repository.save(cliente);
+    }
 }
